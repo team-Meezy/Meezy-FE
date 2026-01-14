4a0ddd9f2b5d0fa7ba06c48f4ebf08d7b1c97368
tccmw <jungmwoo6248@gmail.com>
2026-01-14 14:41:03 +0900

    feat(#1): 디자인 시스템 설정

---
+++ libs/ui/src/design/typography.ts
@@ -0,0 +1,130 @@
+export const typography = {
+  fontFamily: {
+    pretendard: 'Pretendard',
+    gmarketSans: '"GMarketSans", sans-serif',
+  },
+
+  headline: {
+    LHeadlineB: {
+      fontFamily: 'Pretendard',
+      fontSize: '36px',
+      fontWeight: 700,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+    LHeadlineM: {
+      fontFamily: 'Pretendard',
+      fontSize: '36px',
+      fontWeight: 500,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+    HeadlineB: {
+      fontFamily: 'Pretendard',
+      fontSize: '32px',
+      fontWeight: 700,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+    HeadlineM: {
+      fontFamily: 'Pretendard',
+      fontSize: '32px',
+      fontWeight: 500,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+  },
+
+  title: {
+    TitleB: {
+      fontFamily: 'Pretendard',
+      fontSize: '28px',
+      fontWeight: 700,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+    TitleM: {
+      fontFamily: 'Pretendard',
+      fontSize: '28px',
+      fontWeight: 500,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+    sTitleB: {
+      fontFamily: 'Pretendard',
+      fontSize: '24px',
+      fontWeight: 700,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+    sTitleM: {
+      fontFamily: 'Pretendard',
+      fontSize: '24px',
+      fontWeight: 500,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+  },
+
+  body: {
+    LBodyB: {
+      fontFamily: 'Pretendard',
+      fontSize: '20px',
+      fontWeight: 700,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+    LBodyM: {
+      fontFamily: 'Pretendard',
+      fontSize: '20px',
+      fontWeight: 500,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+    BodyB: {
+      fontFamily: 'Pretendard',
+      fontSize: '16px',
+      fontWeight: 700,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+    BodyM: {
+      fontFamily: 'Pretendard',
+      fontSize: '16px',
+      fontWeight: 500,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+  },
+
+  label: {
+    labelB: {
+      fontFamily: 'Pretendard',
+      fontSize: '12px',
+      fontWeight: 700,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+    labelM: {
+      fontFamily: 'Pretendard',
+      fontSize: '12px',
+      fontWeight: 500,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+    slabelB: {
+      fontFamily: 'Pretendard',
+      fontSize: '10px',
+      fontWeight: 700,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+    slabelM: {
+      fontFamily: 'Pretendard',
+      fontSize: '10px',
+      fontWeight: 500,
+      lineHeight: '100%',
+      letterSpacing: '0',
+    },
+  },
+} as const;

059d8fdd2771b6a7089a643f5c50b1036204d2f4
tccmw <jungmwoo6248@gmail.com>
2026-01-15 10:46:27 +0900

    feat(#8) : 팀 참가 모달 구현

--- libs/ui/src/lib/serverModal.tsx
+++ libs/ui/src/lib/serverModal.tsx
@@ -13,8 +13,9 @@ export function ServerModal({
   onClose: () => void;
 }) {
   const [mounted, setMounted] = useState(false);
-  const [createModal, setCreateModal] = useState(false);
+  const [createModal, setCreateModal] = useState(true);
   const [serverName, setServerName] = useState('');
+  const [serverLink, setServerLink] = useState('');
   const [generalError, setGeneralError] = useState('');
   const { previewUrl, fileInputRef, handleClickUpload, handleImageChange } =
     useModalImg();
@@ -80,13 +81,17 @@ export function ServerModal({
           />
         </div>
 
-        {/* 폼 콘텐츠 */}
+        {/* // 폼 콘텐츠 */}
         <div className="p-6 pt-4 flex flex-col gap-6">
           {/* 서버 이름 입력 */}
           <section>
-            <h2 className="text-xl font-bold text-white mb-1">서버 만들기</h2>
+            <h2 className="text-xl font-bold text-white mb-1">
+              {createModal ? '서버 만들기' : '서버 가입하기'}
+            </h2>
             <p className="text-xs text-gray-400 mb-4">
-              회의를 위한 서버를 만들어 보세요!
+              {createModal
+                ? '회의를 위한 서버를 만들어 보세요!'
+                : '초대 받은 서버 가입을 위해 링크를 입력 해주세요.'}
             </p>
             {generalError && (
               <div
@@ -108,53 +113,61 @@ export function ServerModal({
             )}
 
             <label className="block text-xs font-bold text-gray-300 uppercase mb-2">
-              이름
+              {createModal ? '이름' : '링크 입력'}
             </label>
             <input
               type="text"
-              value={serverName}
-              onChange={(e) => setServerName(e.target.value)}
-              placeholder="서버 이름"
+              value={createModal ? serverName : serverLink}
+              onChange={(e) => {
+                if (createModal) {
+                  setServerName(e.target.value);
+                } else {
+                  setServerLink(e.target.value);
+                }
+              }}
+              placeholder={createModal ? '서버 이름' : '초대 받은 링크를 입력'}
               className="w-full bg-[#1e1f22] border-none rounded-md p-3 text-white placeholder:text-gray-500 focus:ring-1 focus:ring-orange-500 outline-none transition-all"
             />
           </section>
 
-          {/* 서버 이미지 지정 */}
-          <section className="border-t border-white/5 pt-3">
-            <h2 className="text-xl font-bold text-white mb-1">
-              서버 대표 이미지 지정
-            </h2>
-            <p className="text-xs text-gray-400 mb-4">
-              서버 대표 이미지를 정해주세요!
-              <br />
-              최소 512 x 512 크기로 지정해주세요.
-            </p>
+          {createModal && (
+            //   {/* 서버 이미지 지정 */}
+            <section className="border-t border-white/5 pt-3">
+              <h2 className="text-xl font-bold text-white mb-1">
+                서버 대표 이미지 지정
+              </h2>
+              <p className="text-xs text-gray-400 mb-4">
+                서버 대표 이미지를 정해주세요!
+                <br />
+                최소 512 x 512 크기로 지정해주세요.
+              </p>
 
-            <input
-              ref={fileInputRef}
-              type="file"
-              accept="image/*"
-              className="hidden"
-              onChange={handleImageChange}
-            />
+              <input
+                ref={fileInputRef}
+                type="file"
+                accept="image/*"
+                className="hidden"
+                onChange={handleImageChange}
+              />
 
-            {previewUrl && (
-              <div className="flex flex-col items-start mb-4">
-                <img
-                  src={previewUrl}
-                  alt="서버 대표 이미지 미리보기"
-                  className="w-42 h-32 rounded-lg object-cover border border-white/10"
-                />
-              </div>
-            )}
+              {previewUrl && (
+                <div className="flex flex-col items-start mb-4">
+                  <img
+                    src={previewUrl}
+                    alt="서버 대표 이미지 미리보기"
+                    className="w-42 h-32 rounded-lg object-cover border border-white/10"
+                  />
+                </div>
+              )}
 
-            <button
-              className="bg-[#ff5c00] hover:bg-[#e65300] text-white text-sm font-bold py-2 px-4 rounded-md transition-colors"
-              onClick={handleClickUpload}
-            >
-              {previewUrl ? '이미지 변경' : '대표 이미지 업로드'}
-            </button>
-          </section>
+              <button
+                className="bg-[#ff5c00] hover:bg-[#e65300] text-white text-sm font-bold py-2 px-4 rounded-md transition-colors"
+                onClick={handleClickUpload}
+              >
+                {previewUrl ? '이미지 변경' : '대표 이미지 업로드'}
+              </button>
+            </section>
+          )}
         </div>
 
         {/* 하단 버튼 바 */}

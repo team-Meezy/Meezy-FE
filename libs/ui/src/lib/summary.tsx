0c31b41e82ecfc3986794f045ef64c3354e76eb5
tccmw <jungmwoo6248@gmail.com>
2026-01-19 23:41:53 +0900

    feat(#32): 회의 요약 페이지 퍼블리싱

--- libs/ui/src/lib/summary.tsx
+++ libs/ui/src/lib/summary.tsx
@@ -1,7 +1,52 @@
+import { typography, colors } from '../design';
+
+const SummaryList = [
+  {
+    SummaryId: 1,
+    title: '효현이 머리카락을 왜 저럴까',
+    date: '2026-01-19',
+    description: '회의 내용',
+  },
+  {
+    SummaryId: 2,
+    title: '효현이 머리카락을 왜 저럴까',
+    date: '2026-01-19',
+    description: '회의 내용',
+  },
+];
+
 export function Summary() {
   return (
-    <div className="flex-[3] border border-white/5 flex flex-col items-center justify-center p-10 gap-3">
-      <h1>회의 요약</h1>
+    <div className="flex-[3] border border-white/5 flex flex-col items-center justify-center p-8 gap-3">
+      <div className="w-full h-full bg-[#1e1e1e] rounded-2xl flex flex-col overflow-y-scroll no-scrollbar">
+        <h1 className="p-6" style={{ ...typography.title.sTitleB }}>
+          회의 요약
+        </h1>
+        <div className="flex flex-col gap-5 px-6">
+          {SummaryList.map((summary) => (
+            <div
+              key={summary.SummaryId}
+              className="max-h-[150px] p-6 rounded-2xl flex flex-col gap-2 overflow-y-scroll no-scrollbar"
+              style={{ backgroundColor: colors.gray[700] }}
+            >
+              <div className="flex gap-2 items-center">
+                <h2 style={{ ...typography.body.BodyB }}>{summary.title}</h2>
+                <p
+                  style={{
+                    ...typography.body.BodyM,
+                    color: colors.primary[500],
+                  }}
+                >
+                  {summary.date}
+                </p>
+              </div>
+              <p style={{ ...typography.label.labelM }}>
+                {summary.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }

2310e299c16c0fa4225bb29cbdca5ca17a367dea
tccmw <jungmwoo6248@gmail.com>
2026-01-21 15:08:35 +0900

    refactor(#38): assets 파일 구조 생성 및 index.ts 엔트리화

---
+++ libs/ui/src/assets/index.ts
@@ -0,0 +1,12 @@
+export { default as ChevronRight } from './ChevronRight.svg';
+export { default as JoinedPlus } from './JoinedPlus.svg';
+export { default as KickMember } from './KickMember.svg';
+export { default as Link } from './Link.svg';
+export { default as Plus } from './Plus.svg';
+export { default as Shrap } from './Shrap.svg';
+export { default as Google } from './Google.svg';
+export { default as Kakao } from './Kakao.svg';
+export { default as Naver } from './Naver.svg';
+export { default as Receive } from './Reseive.png'
+export { default as LoginLogo } from './LoginLogo.png';
+export { default as NoTeamReceive } from './NoTeam_Receive.png'

8b999b38e86146fb4c32d798c0daf252cdb3a5e8
tccmw <jungmwoo6248@gmail.com>
2026-01-21 15:09:54 +0900

    refactor(#38): components index.ts 엔트리화

---
+++ libs/ui/src/lib/components/index.ts
@@ -0,0 +1,6 @@
+export * from './Header';
+export * from './CalendarMockup';
+export * from './DashboardCard';
+export * from './Input';
+export * from './ParticipationChart';
+export * from './SocialButton';

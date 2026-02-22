235e89a855b7f34965b01920a7bbe417871ef761
tccmw <jungmwoo6248@gmail.com>
2026-02-22 22:47:01 +0900

    feat(#56): 팀 전체 조회 api 연동

--- libs/ui/src/lib/sidebar/TeamSidebar.tsx
+++ libs/ui/src/lib/sidebar/TeamSidebar.tsx
@@ -7,24 +7,35 @@ import { useServerCreate } from '../../context';
 import { useServerJoinedTeam } from '../../context';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { useGetTeams } from '@org/shop-data';
+import { useEffect } from 'react';
+
+interface Team {
+  teamId: string;
+  teamName: string;
+  serverImageUrl: string | null;
+}
 
 interface SidebarProps {
   onOpenModal?: () => void;
-  projectSidebarList?: {
-    team_id: number;
-    team_name: string;
-    create_at: null;
-    invite_link: string;
-  }[];
 }
 
-export function TeamSidebar({ onOpenModal, projectSidebarList }: SidebarProps) {
+export function TeamSidebar({ onOpenModal }: SidebarProps) {
   const { imageFile } = useServerCreate();
   const { setJoined } = useServerJoinedTeam();
   const [alarm, setAlarm] = useState(false);
+  const [teams, setTeams] = useState<Team[]>([]);
   const router = useRouter();
 
-  const handleTeamClick = (teamId: number) => {
+  useEffect(() => {
+    const getTeamsData = async () => {
+      const data = await useGetTeams();
+      setTeams(data);
+    };
+    getTeamsData();
+  }, []);
+
+  const handleTeamClick = (teamId: string) => {
     setJoined(true);
     router.push(`/main/${teamId}`);
   };
@@ -36,7 +47,7 @@ export function TeamSidebar({ onOpenModal, projectSidebarList }: SidebarProps) {
         backgroundColor: colors.gray[900],
       }}
     >
-      <div className="mt-12 flex flex-col items-center gap-4 flex-1 overflow-y-auto no-scrollbar w-full">
+      <div className="mt-12 flex flex-col items-center gap-4 flex-1 overflow-y-auto no-scrollbar w-full border-r border-[#262626]">
         <button
           className="min-w-14 min-h-14 flex items-center justify-center rounded-lg border border-gray-800 bg-[#262626] hover:bg-[#252525] transition-colors group"
           aria-label="팀 추가"
@@ -44,9 +55,9 @@ export function TeamSidebar({ onOpenModal, projectSidebarList }: SidebarProps) {
         >
           <Image src={Plus} alt="plus" className="w-5" />
         </button>
-        {projectSidebarList?.map((team) => (
+        {teams?.map((team) => (
           <div
-            key={team.team_id}
+            key={team?.teamId}
             className="flex justify-center items-center mr-4"
           >
             <div
@@ -58,12 +69,21 @@ export function TeamSidebar({ onOpenModal, projectSidebarList }: SidebarProps) {
 
             <button
               type="button"
-              key={team.team_id}
-              aria-label={team.team_name}
-              className="min-w-14 min-h-14 flex items-center justify-center rounded-lg border border-gray-800 bg-[`#262626`] hover:bg-[`#252525`] transition-colors"
-              onClick={() => handleTeamClick(team.team_id)}
+              aria-label={team.teamName}
+              className="min-w-14 min-h-14 flex items-center justify-center rounded-lg border border-gray-800 bg-[#262626] hover:bg-[#252525] transition-colors overflow-hidden"
+              onClick={() => handleTeamClick(team.teamId)}
             >
-              {!imageFile && <div>{team.team_name}</div>}
+              {team.serverImageUrl ? (
+                <Image
+                  src={team.serverImageUrl}
+                  alt={team.teamName}
+                  width={56}
+                  height={56}
+                  className="w-full h-full object-cover"
+                />
+              ) : (
+                <div className="text-xs">{team.teamName}</div>
+              )}
             </button>
           </div>
         ))}

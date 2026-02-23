ec21c27d747614bb1a211119413d772a597ca74b
tccmw <jungmwoo6248@gmail.com>
2026-02-23 16:57:05 +0900

    refactor(#56): 초기 렌더링 시 저장소 수분 흡수 타이밍 문제 해결

--- libs/ui/src/lib/pages/layout/MainRoomWrapper.tsx
+++ libs/ui/src/lib/pages/layout/MainRoomWrapper.tsx
@@ -2,18 +2,13 @@
 
 import { MainRoomPage } from '../MainRoomPage';
 import { useParams } from 'next/navigation';
-import { useServerIdStore } from '@org/shop-data';
-import { useEffect } from 'react';
 
 export function MainRoomWrapper() {
   const params = useParams();
-  const { serverId, setServerId } = useServerIdStore();
-  useEffect(() => {
-    setServerId(params.serverId as string);
-  }, [params.serverId]);
+  const currentServerId = params.serverId as string;
 
-  // 1. URL 파라미터가 아예 없는 경우 방어 로직
-  if (!serverId) {
+  // URL 파라미터가 아예 없는 경우 방어 로직
+  if (!currentServerId) {
     return <div>접근할 수 없는 페이지입니다.</div>;
   }
 

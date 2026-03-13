f585e886a48dc547fd26b1fa95d1008026c8b2e8
tccmw <jungmwoo6248@gmail.com>
2026-03-13 11:07:41 +0900

    fix(#102): 팀원 회의 참가가 동작하지 않는 문제 수정

--- libs/shop/data/src/lib/hooks/meeting/use-join-meeting.ts
+++ libs/shop/data/src/lib/hooks/meeting/use-join-meeting.ts
@@ -2,7 +2,10 @@ import { privateApi } from '../axios';
 
 export const joinMeeting = async (teamId: string) => {
   try {
-    const response = await privateApi.post(`/teams/${teamId}/meetings/join`);
+    const response = await privateApi.post(
+      `/teams/${teamId}/meetings/join`,
+      {}
+    );
     console.log('joinMeeting response', response);
     return response.data;
   } catch (error) {

5b5efa3a1ba10d59e33142aebb6620564113f59b
tccmw <jungmwoo6248@gmail.com>
2026-03-18 15:16:41 +0900

    fix(#176): 프로필 대표 이미지 삭제 기능 미동작 문제 수정

--- libs/shop/data/src/lib/hooks/profile/index.ts
+++ libs/shop/data/src/lib/hooks/profile/index.ts
@@ -2,3 +2,4 @@ export { getMyProfile } from './use-get-my-profile';
 export { deleteAccount } from './use-delete-account';
 export { updatePassword } from './use-update-password';
 export { uploadProfileImage } from './use-upload-profile-image';
+export { deleteProfileImage } from './use-delete-profile-image';
